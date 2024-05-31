828c196ffa423bfacf0fe498d7f0d8e765881d72
Ayush Shrivastava <ayushshrivastava840@gmail.com>
2024-06-01 00:22:47 +0530

    login user

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -5,6 +5,6 @@ import { registerUser } from "../controllers/UserController.js"
 const userRouter = express.Router()
 
 userRouter.post("/register",registerUser)
-userRouter.post("login",loginUser)
+userRouter.post("/login",loginUser)
 
 export default userRouter;
