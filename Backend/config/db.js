7a175a8bd1eb54bf472b7cf5b8595ea84f28db09
Ayush Shrivastava <ayushshrivastava840@gmail.com>
2024-06-04 13:23:20 +0530

    eq

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 import'dotenv/config.js'
 export const connectDB = async()=>{
-    await mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGO_URI).then(()=>console.log("DB Connected"));
 }
