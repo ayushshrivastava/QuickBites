aa1a7d2aa05ce952ea2f208b0c054a145d24b4a3
Ayush Shrivastava <ayushshrivastava840@gmail.com>
2024-08-09 19:51:15 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -41,26 +41,6 @@ const App = () => {
         </div>
     </div>
 );
-
-  return (
-    <>
-    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
-      <div className='w-4/5 mx-auto ' >
-        <Navbar setShowLogin={setShowLogin}/>
-     
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/cart' element={<Cart/>} />
-          <Route path='/order' element={<PlaceOrder/>} />
-          <Route path='/verify' element={<Verify/>} />
-          <Route path='/myorders' element={<MyOrders/>} />
-        </Routes>
-        
-      </div>
-      <Footer/>
-    
-    </>
-  )
 }
 
 export default App
